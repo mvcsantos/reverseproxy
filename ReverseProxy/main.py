d9637934e7e39bc2f417b9ece8ae1b67b481a331
Marcus Santos <mvcs@me.com>
2017-01-18 02:56:59 +0000

    - Fixed forward data to the client - Code cleaning

--- ReverseProxy/main.py
+++ ReverseProxy/main.py
@@ -5,8 +5,5 @@ if __name__ == '__main__':
     serverURL = 'pythonprogramming.net'
     netbusHost = 'webservices.nextbus.com'
     server = ReverseProxyServer('localhost')
-    path = "/service/publicXMLFeed?command=agencyList"
-    request = "GET " + path + " HTTP/1.1\nHost: " + netbusHost + "\nAccept-Encoding: gzip, deflate\n\n"
+    server.start()
 
-    while True:
-        time.sleep(1.0)
--- ReverseProxy/proxy.py
+++ ReverseProxy/proxy.py
@@ -2,7 +2,6 @@ import socket
 import sys
 import threading
 import select
-import asyncio
 from queue import Queue
 
 
@@ -49,59 +48,11 @@ class ReverseProxyServer:
             thread.start()
             self.threads.append(thread)
 
-        self.initializeServerSocket()
-
-        #listenRequestsThread = threading.Thread(target=self.startListening)
-        #listenRequestsThread.daemon = True
-        #listenRequestsThread.start()
-        #self.threads.append(listenRequestsThread)
-
-        # start receiving connections
-        #loop = asyncio.get_event_loop()
-        #listening = self.startListening()
-        #loop.run_until_complete(listening)
-
-        self.initServer()
-
-    # initialize the proxy to receive connections
-    # using TCP/IP protocol
-    def initializeServerSocket(self):
-        for res in socket.getaddrinfo(self.HOST, self.PORT, socket.AF_UNSPEC,
-                                      socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-            af, socktype, proto, canonname, sa = res
-            try:
-                self.proxy_socket = socket.socket(af, socktype, proto)
-                self.proxy_socket.setblocking(False)
-            except OSError as msg:
-                self.proxy_socket = None
-                continue
-            try:
-                self.proxy_socket.bind(sa)
-                self.proxy_socket.listen(5)
-            except OSError as msg:
-                self.proxy_socket.close()
-                self.proxy_socket = None
-                continue
-            break
-        if self.proxy_socket is None:
-            print('could not open socket')
-            sys.exit(1)
-
-            '''
-            # Sockets from which we expect to read
-            inputs = [self.proxy_socket]
-            # Sockets to which we expect to write
-            outputs = []
-            # Outgoing message queues (socket:Queue)
-            message_queues = {}
-            self.communication_channels = [inputs, outputs, message_queues]
-        '''
-
-    def initServer(self):
+    def start(self):
         self.proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.proxy_socket.bind((self.HOST, self.PORT))
-        self.proxy_socket.listen(10)
+        self.proxy_socket.listen(5)
 
         kqueue = select.kqueue()
         kevent = select.kevent(self.proxy_socket.fileno(),
@@ -119,36 +70,20 @@ class ReverseProxyServer:
                     conn, _ = self.proxy_socket.accept()
                     self.queue.put(conn)
 
-    # deprecated
-    def startListening2(self):
-        while True:
-            print('Wainting for connections:')
-            conn, addr = self.proxy_socket.accept()
-            with conn:
-                print('Connected by', addr)
-                self.clientHandler(conn, addr)
-
-    def clientHandler(self, conn, addr):
-        self.queue.put(conn)
-
     # process the request's queue
     def process_requests(self):
         while True:
             conn = self.queue.get()
 
-            request = ""
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                request = data
+            request = conn.recv(1024).decode()
 
             if len(request) > 0:
                 # check cache first
                 response = self.get(request)
                 with self.print_lock:
                     print('Response: \n', response, '\n\n')
-                conn.send(request.encode())
+                if response:
+                    conn.send(response)
 
             else:
                 with self.print_lock:
@@ -156,9 +91,6 @@ class ReverseProxyServer:
                     conn.close()
                 #conn.send(b'Not able to receive client data')
             self.queue.task_done()
-
-    def request_data(self, request):
-        self.get(request)
 
     # request data from the NextBus API
     def get(self, request):
